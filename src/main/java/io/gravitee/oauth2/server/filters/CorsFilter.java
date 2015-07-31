dbf5102399d225184bfb8df40c965414c29594fc
tcompiegne <titouan.compiegne@gmail.com>
2015-07-31 09:47:40 +0200

    Set up initial OAuth 2 Server

---
+++ src/main/java/io/gravitee/oauth2/server/filters/CorsFilter.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *******************************************************************************/
+package io.gravitee.oauth2.server.filters;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class CorsFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		 	response.setHeader("Access-Control-Allow-Origin", "*");
+		    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+		    response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Authorization");
+		    response.setHeader("Access-Control-Max-Age", "3600");
+		    if (!"OPTIONS".equals(request.getMethod())) {
+		    	filterChain.doFilter(request, response);
+		    } else {
+		    }
+	}
+
+}
